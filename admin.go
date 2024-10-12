925b45df3e57a20e67aa0a490cd2226b6d303e5f
brivnc <103654258+brivnc@users.noreply.github.com>
2024-10-12 22:49:53 +0200

    Admin

---
+++ admin.go
@@ -0,0 +1,49 @@
+package kafkaclient
+
+import (
+	"context"
+	"fmt"
+	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"log"
+)
+
+type Admin struct {
+	client *kadm.Client
+}
+
+func newAdmin(brokers []string) *Admin {
+	client, err := kgo.NewClient(kgo.SeedBrokers(brokers...))
+	if err != nil {
+		log.Panicf("Unable to create new Client, %v", err)
+	}
+	admin := kadm.NewClient(client)
+	return &Admin{client: admin}
+}
+
+func (a *Admin) topicExists(topic string) bool {
+	ctx := context.Background()
+	topics, err := a.client.ListTopics(ctx, topic)
+	if err != nil {
+		log.Panicf("Topic does not exist: %v", err)
+	}
+	for _, metadata := range topics {
+		if metadata.Topic == topic {
+			fmt.Printf("Found topic: %s\n", topic)
+			return true
+		}
+	}
+	return false
+}
+
+func (a *Admin) createTopic(topic string) {
+	ctx := context.Background()
+	resp, err := a.client.CreateTopic(ctx, 1, 1, nil, topic)
+	if err != nil {
+		log.Panicf("Unable to create topic '%s' due to request error: %v", topic, err)
+	}
+	if resp.Err != nil {
+		log.Panicf("Unable to create topic '%s' due to Kafka error: %v", topic, resp.Err)
+	}
+	fmt.Printf("Created topic '%s'\n", topic)
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.23.1
 require (
 	github.com/google/uuid v1.6.0
 	github.com/twmb/franz-go v1.17.1
+	github.com/twmb/franz-go/pkg/kadm v1.13.0
 )
 
 require (
 	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/twmb/franz-go/pkg/kmsg v1.8.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 )
